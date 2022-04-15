231bff58afaf3fd1b0246bf4f6685609540e4542
Maksim Kylchinsky <lesteff2016@gmail.com>
2022-04-16 00:10:55 +0300

    added func_lambda.py

---
+++ func_lambda.py
@@ -0,0 +1,18 @@
+func_name = lambda a: f'Hello, {a}'
+print(func_name(input('Введите имя : ')))
+
+
+# Home_work 09
+# Задание 01
+
+# string = ['max', 'back', 'go', 'vent']
+#
+# x = enumerate(string, 1)
+# print(list(x))
+
+# Задание 02
+# double_dict_keys = lambda **kwargs: {f'{key}{key}': value for key, value in kwargs.items()}
+# print(double_dict_keys(a=1))
+
+
+
