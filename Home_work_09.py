3d5a16e526fae594397965cfc61ca9f1525edb4a
Maksim Kylchinsky <lesteff2016@gmail.com>
2022-04-17 20:16:56 +0300

    fix lesson 03

--- Home_work_09.py
+++ Home_work_09.py
@@ -1,41 +1,38 @@
+from typing import List
+from functools import wraps, reduce
+
 # Задание 01
+
 
 string = ['max', 'back', 'go', 'vent']
 x = enumerate(string, 1)
 print(list(x))
-
-
 
 # Задание 02
 double_dict_keys = lambda **kwargs: {f'{key}{key}': value for key, value in kwargs.items()}
 print(double_dict_keys(a=1))
 
 
-
-
-
 # Задание 03
 
 def numbers_decorator(func):
-    def wrapp():
+    def wrapp(numbers):
         print('Производим зачистку четных элементов списка !')
-        func()
+        numbers = list(filter(lambda x: x % 2, numbers))
+        result = func(numbers)
         print('Сделано!')
+        return result
     return wrapp
 
 
-
 @numbers_decorator
-def func_filters():
-    a = [10, 20, 22, 1, 3, 5, 7]
-    print(a)
-    x = list(filter(lambda x: int(x) % 2, a))
-    print(x)
+def func_filters(numbers: List[int]):
+    summ = reduce(lambda a, x: a + x, numbers, 0)
+    return summ
 
 
-func_filters()
-
-
+called_func = func_filters([10, 20, 22, 1, 3, 5, 7])
+print(called_func)
 
 
 # Задание 04
@@ -49,8 +46,6 @@ def change_decorator(func):
     return wrapp
 
 
-
-
 @change_decorator
 def replacement_args(**kwargs):
     kwargs = {"kwargs_1": "Shark", "kwargs_2": "Max"}
@@ -60,5 +55,3 @@ def replacement_args(**kwargs):
 
 
 replacement_args()
-
-
